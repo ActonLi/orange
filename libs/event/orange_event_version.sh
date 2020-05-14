39a8d899f99afa0c55a3465301b0fddd9e498561
liyeqiang <orange-yeqiang.li@cn.abb.com>
2020-05-14 14:37:45 +0800

    calculate build number

--- libs/event/orange_event_version.sh
+++ libs/event/orange_event_version.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+
 BUILD_NUM_FILE="build-number.txt"
 BUILD_NUM=0
 if [ -f ${BUILD_NUM_FILE} ] ; then
 	BUILD_NUM=`cat ${BUILD_NUM_FILE}`
 fi
 echo -n "BuildNum: "
-let BUILD_NUM=${BUILD_NUM}+1
+BUILD_NUM=`expr ${BUILD_NUM} + 1`
 echo ${BUILD_NUM} > ${BUILD_NUM_FILE}
 echo "#include \"orange_event_version.h\"" > orange_event_version.c
 echo "int orange_event_build_num = ${BUILD_NUM};" >> orange_event_version.c
