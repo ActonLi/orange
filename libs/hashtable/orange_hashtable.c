307e8e2af369da19120461f59300506661b4da71
ActonLi <lyq791007544@icloud.com>
2020-10-12 15:49:24 +0800

    update hashtable

--- libs/hashtable/orange_hashtable.c
+++ libs/hashtable/orange_hashtable.c
@@ -8,10 +8,143 @@
 
 ORANGE_VERSION_GENERATE(orange_hashtable, 1, 1, 1, ORANGE_VERSION_TYPE_ALPHA);
 
+orange_hash_table_t* orange_hashtable_create(int modulus, orange_hashtable_hashfunc* hasher, 
+        orange_hashtable_keycmp* cmp, orange_hashtable_hashfree* keyfree, orange_hashtable_hashfree* datafree)
+{
+    orange_hash_table_t* new = malloc(sizeof(struct orange_hash_table));
+    if (NULL == new) {
+        return NULL;
+    }
+
+    new->modulus   = modulus;
+    new->hasher    = hasher;
+    new->keycmp    = cmp;
+    new->keyfree   = keyfree;
+    new->datafree  = datafree;
+    new->items     = malloc(modulus * sizeof(struct orange_hashtable_item));
+    if (NULL == new->items) {
+        free(new);
+        return NULL;
+    }
+    return new;
+}
+
+void orange_hashtable_destroy(orange_hash_table_t* tbl)
+{
+    orange_hashtable_item_t *item, *next;
+    int       slot;
+    if (NULL == tbl) {
+        return;
+    }
+
+    for (slot = 0; slot < tbl->modulus; slot++) {
+        for (item = tbl->items[slot]; item;) {
+            next = item->next;
+            if (tbl->keyfree) {
+                tbl->keyfree((void*)item->key);
+            }
+            if (tbl->datafree) {
+                tbl->datafree(item->data);
+            }
+            free(item);
+            item = next;
+        }
+    }
+
+    free(tbl);
+}
+
+int orange_hashtable_add(const void* key, void* data, orange_hash_table_t* tbl)
+{
+    orange_hashtable_item_t* new = malloc(sizeof(orange_hashtable_item_t));
+    orange_hashtable_item_t** item;
+    int        slot;
+    if (NULL == new) {
+        return 1;
+    }
+
+    new->key  = key;
+    new->data = data;
+    slot      = tbl->hasher(key) % tbl->modulus;
+    for (item = &tbl->items[slot]; *item; item = &(*item)->next);
+    *item = new;
+
+    return 0;
+}
+
+void orange_hashtable_remove(const void* key, orange_hash_table_t* tbl)
+{
+    orange_hashtable_item_t **pitem, *item;
+    int        slot;
+    slot = tbl->hasher(key) % tbl->modulus;
+    for (pitem = &tbl->items[slot]; *pitem; pitem = &(*pitem)->next) {
+        if (0 == tbl->keycmp(key, (*pitem)->key)) {
+            item  = *pitem;
+            *pitem = (*pitem)->next;
+            if (tbl->keyfree) {
+                tbl->keyfree((void*)item->key);
+            }
+            if (tbl->datafree) {
+                tbl->datafree(item->data);
+            }
+            if (!tbl->use_arena) {
+                free(item);
+            }
+            break;
+        }
+    }
+}
+
+void* orange_hashtable_find(const void* key, orange_hash_table_t* tbl)
+{
+    int       slot = tbl->hasher(key) % tbl->modulus;
+    orange_hashtable_item_t* item;
+    for (item = tbl->items[slot]; item; item = item->next) {
+        if (0 == tbl->keycmp(key, item->key)) {
+            return item->data;
+        }
+    }
+    return NULL;
+}
+
+static void __orange_hashtable_iter_next_slot(orange_hash_table_t* tbl)
+{
+    while (tbl->iter.next == NULL) {
+        tbl->iter.slot++;
+        if (tbl->iter.slot == tbl->modulus) {
+            break;
+        }
+        tbl->iter.next = tbl->items[tbl->iter.slot];
+    }
+
+    return;
+}
+
+void orange_hashtable_iter_init(orange_hash_table_t* tbl)
+{
+    tbl->iter.slot = 0;
+    tbl->iter.next = tbl->items[tbl->iter.slot];
+    if (NULL == tbl->iter.next) {
+        __orange_hashtable_iter_next_slot(tbl);
+    }
+    return;
+}
+
+void* orange_hashtable_iterate(orange_hash_table_t* tbl)
+{
+    orange_hashtable_item_t* this = tbl->iter.next;
+    if (this) {
+        tbl->iter.next = this->next;
+        if (NULL == tbl->iter.next) {
+            __orange_hashtable_iter_next_slot(tbl);
+        }
+    }
+    return this ? this->data : NULL;
+}
+
 static int __orange_hashtable_module_init(void)
 {
-
-	snprintf(orange_hashtable_description, 127, "Orange Event Module " ORANGE_VERSION_FORMAT "-%s #%u: %s", ORANGE_VERSION_QUAD(orange_hashtable_version),
+	snprintf(orange_hashtable_description, 127, "Orange Hash Table Module " ORANGE_VERSION_FORMAT "-%s #%u: %s", ORANGE_VERSION_QUAD(orange_hashtable_version),
 			 orange_version_type(orange_hashtable_version_type), orange_hashtable_build_num, orange_hashtable_build_date);
 
 	orange_log(ORANGE_LOG_INFO, "%s\n", orange_hashtable_description);
@@ -21,7 +154,7 @@ static int __orange_hashtable_module_init(void)
 
 static void __orange_hashtable_module_fini(void)
 {
-	orange_log(ORANGE_LOG_INFO, "Orange Event Module unloaded.\n");
+	orange_log(ORANGE_LOG_INFO, "Orange Hash Table Module unloaded.\n");
 
 	return;
 }
--- libs/hashtable/orange_hashtable.h
+++ libs/hashtable/orange_hashtable.h
@@ -6,4 +6,38 @@
 ORANGE_VERSION_TYPE(orange_hashtable);
 ORANGE_DEFINE_MODULE_EXTENSION(orange_hashtable);
 
+typedef struct orange_hashtable_item {
+    const void*       key;
+    void*             data;
+    struct orange_hashtable_item* next;
+} orange_hashtable_item_t;
+
+typedef unsigned int orange_hashtable_hashfunc(const void* key);
+typedef int          orange_hashtable_keycmp(const void* a, const void* b);
+typedef void         orange_hashtable_hashfree(void* p);
+
+typedef struct orange_hash_table {
+    unsigned int                        modulus;
+    struct orange_hashtable_item**      items;
+    orange_hashtable_hashfunc*          hasher;
+    orange_hashtable_keycmp*            keycmp;
+    int                                 use_arena;
+    orange_hashtable_hashfree*          keyfree;
+    orange_hashtable_hashfree*          datafree;
+
+    struct {
+        struct orange_hashtable_item*   next;
+        unsigned int                    slot;
+    } iter;
+} orange_hash_table_t;
+
+orange_hash_table_t* orange_hashtable_create(int modulus, orange_hashtable_hashfunc*, 
+        orange_hashtable_keycmp*, orange_hashtable_hashfree*, orange_hashtable_hashfree*);
+void orange_hashtable_destroy(orange_hash_table_t*);
+int orange_hashtable_add(const void* key, void* data, orange_hash_table_t*);
+void orange_hashtable_remove(const void* key,orange_hash_table_t* tbl);
+void* orange_hashtable_find(const void* key, orange_hash_table_t*);
+void orange_hashtable_iter_init(orange_hash_table_t*);
+void* orange_hashtable_iterate(orange_hash_table_t*);
+
 
