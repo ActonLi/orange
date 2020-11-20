0c3921a5b198d01c24c54be6fddda5402e107e2e
liyeqiang <orange_li@outlook.com>
2020-11-20 13:57:38 -0800

    thread task

--- libs/thread_task/orange_thread_task.c
+++ libs/thread_task/orange_thread_task.c
@@ -8,6 +8,15 @@
 
 ORANGE_VERSION_GENERATE(orange_thread_task, 1, 1, 1, ORANGE_VERSION_TYPE_ALPHA);
 
+typedef struct orange_thread_task_entry {
+    char                                    task_name[ORANGE_THREAD_TASK_NAME_LEN_MAX];
+    pthread_cond_t                          thread_cond;
+	pthread_mutex_t                         thread_evmutex;
+	pthread_condattr_t                      thread_cattr;
+	uint32                                  event_flag;
+    RB_ENTRY(orange_thread_task_entry)      entry;
+} orange_thread_task_entry_t; 
+
 static int __orange_thread_task_module_init(void)
 {
 	snprintf(orange_thread_task_description, 127, "Orange thread task module " ORANGE_VERSION_FORMAT "-%s #%u: %s", ORANGE_VERSION_QUAD(orange_thread_task_version),
@@ -23,6 +32,41 @@ static void __orange_thread_task_module_fini(void)
 	orange_log(ORANGE_LOG_INFO, "Orange thread task module unloaded.\n");
 
 	return;
+}
+
+int orange_thread_task_create(struct orange_thread_task_parameters *task_params, void* init_params, void* task_arg)
+{
+    int ret;
+    pthread_t thread_id;
+    pthread_attr_t thread_attr;  
+    struct sched_param thead_sched;
+
+
+    if (NULL == task_params || NULL == task_params->task_handler) {
+        ret = -1;
+        goto exit;
+    }
+
+    if (task_params->task_init) {
+        task_params->task_init(init_params);
+    }
+
+    thread_sched.sched_priority = task_params->priority > 99 ? 99 : task_params->priority;
+    thread_sched.sched_priority = thread_sched.sched_priority < 1 ? 1 : thread_sched.sched_priority;
+
+    pthread_attr_init(&thread_attr);
+    pthread_attr_setschedpolicy(&thread_attr,  SCHED_RR);
+    pthread_attr_setinheritsched(&thread_attr, PTHREAD_EXPLICIT_SCHED);
+    pthread_attr_setstacksize(&thread_attr, task_params->stack_size <= 65536 ? 65536 : task_params->stack_size);
+    pthread_attr_setschedparam (&thread_attr,  &thread_sched);
+
+    ret = pthread_create(&thread_id, &thread_attr, task_params->task_handler, task_arg);
+    if (0 != ret) {
+        pthread_attr_destroy(&thread_attr);
+    }
+
+exit:
+    return ret;
 }
 
 static int orange_thread_task_modthread_task(orange_module_t mod, int type, void* data)
